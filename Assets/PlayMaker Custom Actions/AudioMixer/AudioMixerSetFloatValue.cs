652ece21e00d589e893be869e17676f9beb1f603
jeanfabre <contact@fabrejean.net>
2015-09-24 13:22:40 +0300

    new actions to set exposed float value from audio mixers

---
+++ Assets/PlayMaker Custom Actions/AudioMixer/AudioMixerSetFloatValue.cs
@@ -0,0 +1,64 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+// original actions from Tropical: http://hutonggames.com/playmakerforum/index.php?topic=11218.msg52963#msg52963
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+
+using UnityEngine;
+using UnityEngine.Audio;
+using HutongGames.PlayMaker;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Audio)]
+	[Tooltip("Sets the float value of an exposed parameter for a Unity Audio Mixer.")]
+	public class AudioMixerSetFloatValue : FsmStateAction
+	{
+		[RequiredField]
+		[ObjectType(typeof(AudioMixer))]
+		[Tooltip("The Audio Mixer with the exposed parameter.")]
+		public FsmObject theMixer;
+
+		[RequiredField]
+		[Tooltip("The name of the exposed parameter.")]
+		[Title("Name of Parameter")]
+		public FsmString exposedFloatName;
+
+		[Title("Float Value")]
+		public FsmFloat floatvalue;
+		public bool everyFrame;
+		
+		public override void Reset()
+		{
+			theMixer = null;
+			exposedFloatName = null;
+			floatvalue = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoSetMixerFloatValue();
+			
+			if (!everyFrame)
+				Finish();
+		}
+
+		public override void OnUpdate()
+		{
+			DoSetMixerFloatValue();
+		}
+		
+		public void DoSetMixerFloatValue()
+		{
+			var _TheMixer = theMixer.Value as AudioMixer;
+
+			if (_TheMixer!=null && !string.IsNullOrEmpty(exposedFloatName.Value))
+			{
+				var _ExposedFloatName = exposedFloatName.Value;
+				var _Floatvalue = floatvalue.Value;
+
+				_TheMixer.SetFloat (_ExposedFloatName, _Floatvalue);
+			}
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/AudioMixer/AudioMixerSetMultiFloatValues.cs
@@ -0,0 +1,67 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
+// original actions from Tropical: http://hutonggames.com/playmakerforum/index.php?topic=11218.msg52963#msg52963
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using UnityEngine.Audio;
+using HutongGames.PlayMaker;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Audio)]
+	[Tooltip("Sets the float values of multiple exposed parameters for a Unity Audio Mixer.")]
+	public class AudioMixerSetMultiFloatValues : FsmStateAction
+	{
+		[RequiredField]
+		[ObjectType(typeof(AudioMixer))]
+		[Tooltip("The Audio Mixer with the exposed parameter(s).")]
+		public FsmObject theMixer;
+
+		[CompoundArray("Count", "Name of Parameter", "Float Value")]
+		[RequiredField]
+		[Tooltip("The name of the exposed parameter.")]
+		public FsmString[] exposedFloatName;
+		public FsmFloat[] floatvalue;
+		public bool everyFrame;
+
+		private string _ExposedFloatName;
+		private float _Floatvalue;
+
+		public override void Reset()
+		{
+			theMixer = null;
+			exposedFloatName = new FsmString[1];
+			floatvalue = new FsmFloat[1];
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoSetMixerFloatValues();
+			
+			if (!everyFrame)
+				Finish();
+		}
+
+		public override void OnUpdate()
+		{
+			DoSetMixerFloatValues();
+		}
+		
+		public void DoSetMixerFloatValues()
+		{
+			var _TheMixer = theMixer.Value as AudioMixer;
+
+			if (_TheMixer!=null)
+			{
+				for (int i = 0; i<exposedFloatName.Length;i++)
+				{
+					if(!exposedFloatName[i].IsNone || !exposedFloatName[i].Value.Equals("")) 
+						_ExposedFloatName = exposedFloatName[i].Value;
+						_Floatvalue = floatvalue[i].IsNone ? 0f : floatvalue[i].Value;
+						_TheMixer.SetFloat (_ExposedFloatName, _Floatvalue);
+				}
+			}
+		}
+	}
+}
