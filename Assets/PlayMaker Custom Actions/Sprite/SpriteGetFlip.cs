e6732593590aa79541d2eada4b9f0c92bec849e0
Jean Fabre <contact@fabrejean.net>
2017-04-21 09:25:20 +0300

    new actions to get set sprite flip values

---
+++ Assets/PlayMaker Custom Actions/Sprite/SpriteGetFlip.cs
@@ -0,0 +1,76 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System.Collections;
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("sprite")]
+	[Tooltip("Gets the Flips values of a Sprite. Object must have a Sprite Renderer.")]
+	public class SpriteGetFlip : FsmStateAction
+	{
+
+		[RequiredField]
+		[Tooltip("The SpriteRenderer to control.")]
+		[CheckForComponent(typeof(SpriteRenderer))]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("The x flip value")]
+		[UIHint(UIHint.Variable)]
+		public FsmBool flipX;
+
+		[Tooltip("The y flip value")]
+		[UIHint(UIHint.Variable)]
+		public FsmBool flipY;
+
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+		SpriteRenderer spriteRenderer;
+
+
+		public override void Reset()
+		{
+			gameObject = null;
+			flipX = null;
+			flipY = null;
+
+		}
+
+		public override void OnEnter()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return;
+
+			spriteRenderer = go.GetComponent<SpriteRenderer>();
+
+			if (spriteRenderer == null)
+			{
+				LogError("SpriteFlip: Missing SpriteRenderer!");
+				Finish();
+				return;
+			}
+
+			GetFlipSprites();
+
+			if (!everyFrame) 
+			{
+				Finish ();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			GetFlipSprites ();
+		}
+
+		void GetFlipSprites()
+		{
+			flipX.Value = spriteRenderer.flipX;
+			flipY.Value = spriteRenderer.flipY;
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Sprite/SpriteSetFlip.cs
@@ -0,0 +1,101 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System.Collections;
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("sprite")]
+	[Tooltip("Flip a Sprite. Object must have a Sprite Renderer.")]
+	public class SpriteSetFlip : FsmStateAction
+	{
+
+		[RequiredField]
+		[Tooltip("The SpriteRenderer to control.")]
+		[CheckForComponent(typeof(SpriteRenderer))]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("The Flip X value")]
+		public FsmBool flipX;
+
+		[Tooltip("The Flip Y value")]
+		public FsmBool flipY;
+
+		[Tooltip("Reset flip values when state exits")]
+		public FsmBool resetOnExit;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+		SpriteRenderer spriteRenderer;
+		bool flipX_orig;
+		bool flipY_orig;
+
+
+		public override void Reset()
+		{
+			gameObject = null;
+			flipX = null;
+			flipY = null;
+			resetOnExit = false;
+
+		}
+
+		public override void OnEnter()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null) return;
+
+			spriteRenderer = go.GetComponent<SpriteRenderer>();
+
+			if (spriteRenderer == null)
+			{
+				LogError("SpriteFlip: Missing SpriteRenderer!");
+				Finish();
+				return;
+			}
+
+			if (resetOnExit.Value)
+			{
+				flipX_orig = spriteRenderer.flipX;
+				flipY_orig = spriteRenderer.flipY;
+			}
+
+			FlipSprites();
+
+			if (!everyFrame) 
+			{
+				Finish ();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			if (this.Enabled)
+			{
+				
+				FlipSprites ();
+			}
+		}
+
+
+		public override void OnExit()
+		{
+			if (resetOnExit.Value)
+			{
+				spriteRenderer.flipX = flipX_orig;
+				spriteRenderer.flipY = flipY_orig;
+			}
+		}
+		void FlipSprites()
+		{
+			if (spriteRenderer.flipX != flipX.Value) spriteRenderer.flipX = flipX.Value;
+			if (spriteRenderer.flipY != flipY.Value) spriteRenderer.flipY = flipY.Value;
+		}
+
+
+
+	}
+}
