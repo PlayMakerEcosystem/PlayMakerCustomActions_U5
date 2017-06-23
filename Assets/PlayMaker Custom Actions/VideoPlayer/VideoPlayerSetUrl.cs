1aa321c4763f0e6fb781f6e130da1b9ceb85048f
Jean Fabre <contact@fabrejean.net>
2017-06-23 10:42:52 +0300

    added VideoPlayerSetUrl action

---
+++ Assets/PlayMaker Custom Actions/VideoPlayer/VideoPlayerSetUrl.cs
@@ -0,0 +1,65 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2017. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+#if UNITY_5_6_OR_NEWER
+
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("VideoPlayer")]
+	[Tooltip("Sets the url value of a VideoPlayer.")]
+	public class VideoPlayerSetUrl : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(VideoPlayer))]
+		[Tooltip("The GameObject with an VideoPlayer component.")]
+		public FsmOwnerDefault gameObject;
+
+		[RequiredField]
+		[Tooltip("The url Value")]
+		public FsmString url;
+
+		GameObject go;
+
+		VideoPlayer _vp;
+
+
+		public override void Reset()
+		{
+			gameObject = null;
+			url = null;
+		}
+
+		public override void OnEnter()
+		{
+			GetVideoPlayer ();
+
+			ExecuteAction ();
+
+			Finish ();
+
+		}
+
+
+		void ExecuteAction()
+		{
+			if (_vp != null)
+			{
+				_vp.url = url.Value;
+			}
+		}
+
+		void GetVideoPlayer()
+		{
+			go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go != null)
+			{
+				_vp = go.GetComponent<VideoPlayer>();
+			}
+		}
+	}
+}
+
+#endif
