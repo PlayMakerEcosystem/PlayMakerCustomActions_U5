eac574a2847bffe86efb3b18150c5e3285f928ad
Jean Fabre <contact@fabrejean.net>
2017-12-27 11:57:23 +0300

    brought in standard assets so that cloning is easier, plus safe commit

---
+++ Assets/PlayMaker Custom Actions/__Internal/FsmStateActionAdvanced.cs
@@ -0,0 +1,83 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	public abstract class FsmStateActionAdvanced : FsmStateAction
+	{
+		
+		public enum FrameUpdateSelector {OnUpdate,OnLateUpdate,OnFixedUpdate};
+
+		[ActionSection("Update type")]
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+		
+		public FrameUpdateSelector updateType;
+		
+		
+		public abstract void OnActionUpdate();
+		
+		public override void Reset()
+		{
+			everyFrame = false;
+			updateType = FrameUpdateSelector.OnUpdate;
+		}
+
+		public override void OnPreprocess()
+		{
+			if (updateType == FrameUpdateSelector.OnFixedUpdate)
+			{
+				Fsm.HandleFixedUpdate = true;
+			}
+
+			#if PLAYMAKER_1_8_5_OR_NEWER
+			if (updateType == FrameUpdateSelector.OnLateUpdate)
+			{
+				Fsm.HandleLateUpdate = true;
+			}
+			#endif
+		}
+
+		
+		public override void OnUpdate()
+		{
+			if (updateType == FrameUpdateSelector.OnUpdate)
+			{
+				OnActionUpdate();
+			}
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		public override void OnLateUpdate()
+		{
+			if (updateType == FrameUpdateSelector.OnLateUpdate)
+			{
+				OnActionUpdate();
+			}
+
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+		public override void OnFixedUpdate()
+		{
+			if (updateType == FrameUpdateSelector.OnFixedUpdate)
+			{
+				OnActionUpdate();
+			}
+
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+	}
+}
