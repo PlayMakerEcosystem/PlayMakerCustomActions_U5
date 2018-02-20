4a8b0ffbac573e3e3edecd9654f722865588246c
Jean Fabre <contact@fabrejean.net>
2018-02-20 10:33:29 +0300

    New Action Overlap2d from Physics 2d from Digitom

---
+++ Assets/PlayMaker Custom Actions/Physics 2d/Editor/Overlap2dEditor.cs
@@ -0,0 +1,224 @@
+﻿using HutongGames.PlayMaker.Actions;
+using HutongGames.PlayMakerEditor;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using System.Collections;
+
+namespace HutongGames.PlayMakerEditor
+{
+    [CustomActionEditor(typeof(Overlap2d))]
+    public class Overlap2dEditor : CustomActionEditor
+    {
+        private Vector2 pos;
+        private GameObject go;
+        private Overlap2d action;
+
+        //Area vars
+        private Vector2 handleAreaPointA;
+        private Vector2 handleAreaPointB;
+        private Vector2 center;
+        private Vector2 size;
+        private float sizeX;
+        private float sizeY;
+
+        //Capsule vars
+        private Vector2 disc1Center;
+        private Vector2 disc2Center;
+        private float discDistance;
+        private Vector2 capEdgeSize;
+
+        //point vars
+        private Vector2 handlePoint;
+
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            action = target as Overlap2d;
+
+            go = action.Fsm.GetOwnerDefaultTarget(action.position);
+
+            
+            if (go)
+            {
+                pos = go.transform.position;
+                //Initial setup for Area points
+                handleAreaPointA = action.pointA.Value + pos;
+                handleAreaPointB = action.pointB.Value + pos;
+                //Initial setup for point
+                handlePoint = action.point.Value + pos;
+            }
+
+        }
+
+        public override bool OnGUI()
+        {
+            GUILayout.Label("Setup:", EditorStyles.boldLabel);
+            EditField("position");
+
+            GUILayout.Space(10);
+
+            EditField("shape");
+
+            switch (action.shape)
+            {
+                case Overlap2d.ShapeType.Circle:
+                    EditField("radius");
+                    EditField("offset");
+                    break;
+                case Overlap2d.ShapeType.Box:
+                    EditField("boxSize");
+                    EditField("offset");
+                    break;
+                case Overlap2d.ShapeType.Area:
+                    EditField("pointA");
+                    EditField("pointB");
+                    break;
+                case Overlap2d.ShapeType.Capsule:
+                    EditField("direction");
+                    EditField("size");
+                    EditField("offset");
+                    break;
+                case Overlap2d.ShapeType.Point:
+                    EditField("point");
+                    break;
+            }
+
+            GUILayout.Space(10);
+
+            EditField("minDepth");
+            EditField("maxDepth");
+
+            GUILayout.Label("Results:", EditorStyles.boldLabel);
+            EditField("storeDidHit");
+            EditField("storeGameObjects");
+            EditField("everyFrame");
+
+            GUILayout.Label("Filter:", EditorStyles.boldLabel);
+            int temp = EditorGUILayout.MaskField("Mask", InternalEditorUtility.LayerMaskToConcatenatedLayersMask(action.layerMask.Value), InternalEditorUtility.layers);
+            action.layerMask.Value = InternalEditorUtility.ConcatenatedLayersMaskToLayerMask(temp);
+
+            GUILayout.Label("Debug:", EditorStyles.boldLabel);
+            EditField("debug");
+            if (action.debug.Value)
+                EditField("debugColor");
+
+
+            return GUI.changed;
+
+        }
+
+        public override void OnSceneGUI()
+        {
+
+            if (!go || !action.debug.Value)
+                return;
+
+            Handles.color = action.debugColor.Value;
+
+            switch (action.shape)
+            {
+                case Overlap2d.ShapeType.Circle:
+                    Handles.DrawWireDisc(pos + action.offset.Value, Vector3.back, action.radius.Value);
+                    break;
+                case Overlap2d.ShapeType.Box:
+                    Handles.DrawWireCube(pos + action.offset.Value, action.boxSize.Value);
+                    break;
+                case Overlap2d.ShapeType.Area:
+                    DrawAreaShape();
+                    break;
+                case Overlap2d.ShapeType.Capsule:
+                    DrawCapsuleShape();
+                    break;
+                case Overlap2d.ShapeType.Point:
+                    DrawPointShape();
+                    break;
+            }
+
+            pos = go.transform.position;
+        }
+
+        void DrawCapsuleShape()
+        {
+            float discRadius = action.size.Value.x * 0.5f;
+            if (action.direction == CapsuleDirection2D.Vertical)
+            {
+                //disc 1
+                disc1Center = new Vector2(action.offset.Value.x + pos.x, (action.offset.Value.y + pos.y) + (action.size.Value.y * 0.5f));
+                //disc2
+                disc2Center = new Vector2(action.offset.Value.x + pos.x, (action.offset.Value.y + pos.y) - (action.size.Value.y * 0.5f));
+                //rectangle
+                discDistance = Vector2.Distance(disc1Center, disc2Center);
+                capEdgeSize = new Vector2(action.size.Value.x, discDistance);
+            }
+            else
+            {
+                //disc 1
+                disc1Center = new Vector2((action.offset.Value.x + pos.x) + (action.size.Value.y * 0.5f), action.offset.Value.y + pos.y);
+                //disc2
+                disc2Center = new Vector2((action.offset.Value.x + pos.x) - (action.size.Value.y * 0.5f), action.offset.Value.y + pos.y);
+                //rectangle
+                discDistance = Vector2.Distance(disc1Center, disc2Center);
+                capEdgeSize = new Vector2(discDistance, action.size.Value.x);
+            }
+            Handles.DrawWireDisc(disc1Center, Vector3.back, discRadius);
+            Handles.DrawWireDisc(disc2Center, Vector3.back, discRadius);
+            Handles.DrawWireCube(action.offset.Value + pos, capEdgeSize);
+
+        }
+
+        void DrawAreaShape()
+        {
+            EditorGUI.BeginChangeCheck();
+            //have handles follow go posiiton if we move it in the scene
+            if (handleAreaPointA != action.pointA.Value || handleAreaPointB != action.pointB.Value)
+            {
+                handleAreaPointA = Handles.PositionHandle(action.pointA.Value + pos, Quaternion.identity);
+                handleAreaPointB = Handles.PositionHandle(action.pointB.Value + pos, Quaternion.identity);
+            }
+            else //move the handles normally in the scene
+            {
+                handleAreaPointA = Handles.PositionHandle(handleAreaPointA, Quaternion.identity);
+                handleAreaPointB = Handles.PositionHandle(handleAreaPointB, Quaternion.identity);
+            }
+
+            if (EditorGUI.EndChangeCheck())//update the scipt values after dragging
+            {
+                action.pointA.Value = handleAreaPointA - pos;
+                action.pointB.Value = handleAreaPointB - pos;
+            }
+
+            //draw pin points on the corners
+            Handles.DrawSolidDisc(pos + action.pointA.Value, Vector3.back, 0.03f);
+            Handles.DrawSolidDisc(pos + action.pointB.Value, Vector3.back, 0.03f);
+
+            //parameters for the cube layout
+            center = Vector2.Lerp(pos + action.pointA.Value, pos + action.pointB.Value, 0.5f);
+            sizeX = Vector2.Distance(Vector2.left * action.pointA.Value.x, Vector2.left * action.pointB.Value.x);
+            sizeY = Vector2.Distance(Vector2.up * action.pointA.Value.y, Vector2.up * action.pointB.Value.y);
+            size = new Vector2(sizeX, sizeY);
+
+            //draw the cube
+            Handles.DrawWireCube(center, size);
+        }
+
+        void DrawPointShape()
+        {
+            EditorGUI.BeginChangeCheck();
+            if (handlePoint != action.point.Value) //point follows go if we move go
+                handlePoint = Handles.PositionHandle(action.point.Value + pos, Quaternion.identity);
+            else //move point normally in scene view
+                handlePoint = Handles.PositionHandle(handlePoint, Quaternion.identity);
+
+            if (EditorGUI.EndChangeCheck()) //update script values after dragging
+                action.point.Value = handlePoint - pos;
+
+            //draw a cross-hair to see the point better
+            Handles.DrawLine(pos + action.point.Value - (Vector2.left * 0.3f), pos + action.point.Value + (Vector2.left * 0.3f));
+            Handles.DrawLine(pos + action.point.Value - (Vector2.up * 0.3f), pos + action.point.Value + (Vector2.up * 0.3f));
+            Handles.DrawWireDisc(pos + action.point.Value, Vector3.back, 0.075f);
+        }
+    }
+}
---
+++ Assets/PlayMaker Custom Actions/Physics 2d/Overlap2d.cs
@@ -0,0 +1,169 @@
+﻿// License: Attribution 4.0 International (CC BY 4.0)
+//Author: Digitom: https://hutonggames.com/playmakerforum/index.php?topic=18337.0;topicseen
+/*--- __ECO__ __PLAYMAKER__ __ACTION__
+EcoMetaStart
+{
+"script dependancies":[
+						"Assets/PlayMaker Custom Actions/Physics 2d/Editor/Overlap2dEditor.cs"
+					  ]
+}
+EcoMetaEnd
+---*/
+
+using System;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+
+	[ActionCategory(ActionCategory.Physics2D)]
+	[Tooltip("Bool check to see if the overlap is intersecting with other colliders. ")]
+	public class Overlap2d : FsmStateAction
+	{
+
+        public enum ShapeType { Circle, Box, Area, Capsule, Point }
+
+        [Tooltip("Position of the overlap shape. default position will be owner's transform.")]
+        public FsmOwnerDefault position;
+
+        [Tooltip("Offset position of shape.")]
+        public FsmVector2 offset;
+
+        [Tooltip("Type of overlap shape.")]
+        public ShapeType shape;
+
+        //CIRCLE
+        [Tooltip("Size of the circle.")]
+        public FsmFloat radius;
+
+        //BOX
+        [Tooltip("Size of the box.")]
+        public FsmVector2 boxSize;
+
+        //AREA
+        [Tooltip("One corner of the rectangle.")]
+        public FsmVector2 pointA;
+        [Tooltip("Diagonally opposite the point A corner of the rectangle.")]
+        public FsmVector2 pointB;
+
+        //CAPSULE
+        [Tooltip("Size of the capsule.")]
+        public FsmVector2 size;
+        [Tooltip("Direction of the capsule.")]
+        public CapsuleDirection2D direction;
+
+        //POINT
+        [Tooltip("point in space.")]
+        public FsmVector2 point;
+
+        //MUTUAL EFFECTORS
+        [Tooltip("Only include objects with a Z coordinate (depth) greater than or equal to this value.")]
+        public FsmFloat minDepth;
+        [Tooltip("Only include objects with a Z coordinate (depth) less than or equal to this value.")]
+        public FsmFloat maxDepth;
+
+        [Tooltip("Set a bool variable to true if hit something, otherwise false.")]
+		[UIHint(UIHint.Variable)]
+		public FsmBool storeDidHit;
+
+        [Tooltip("Store all GameObjects in the overlap area.")]
+        [UIHint(UIHint.Variable)]
+        [ArrayEditor(VariableType.GameObject)]
+        public FsmArray storeGameObjects;
+
+        [Tooltip("Repeat every frame. Typically this would be set to True.")]
+        public bool everyFrame;
+		
+		[UIHint(UIHint.Layer)]
+		[Tooltip("Detect only from these layers.")]
+		public FsmInt layerMask;
+		
+		[Tooltip("Draw a debug shape. Note: Check Gizmos in the Game View to see it in game.")]
+		public FsmBool debug;
+
+        [Tooltip("The color to use for the debug line.")]
+        public FsmColor debugColor;
+
+        private Vector2 pos;
+        private Collider2D hit;
+        private Collider2D[] collidedObjects;
+
+		public override void Reset()
+		{
+
+			position = null;
+            shape = ShapeType.Box;
+			storeDidHit = null;
+            storeGameObjects = null;
+			layerMask = new FsmInt();
+			debugColor = Color.cyan;
+			debug = true;
+		}
+		
+		public override void OnEnter()
+		{
+
+			DoOverlap();
+
+            if (!everyFrame)
+            {
+                Finish();
+            }
+
+        }
+
+        public override void OnUpdate()
+        {
+            DoOverlap();
+        }
+
+        private void DoOverlap()
+		{
+
+            var go = Fsm.GetOwnerDefaultTarget(position);
+            if (go)
+            {
+                pos = go.transform.position;
+            }
+
+            switch (shape)
+            {
+                case ShapeType.Circle:
+                    collidedObjects = Physics2D.OverlapCircleAll(pos + offset.Value, radius.Value, layerMask.Value,minDepth.Value,maxDepth.Value);
+                    break;
+                case ShapeType.Box:
+                    collidedObjects = Physics2D.OverlapBoxAll(pos + offset.Value, boxSize.Value, 0, layerMask.Value, minDepth.Value, maxDepth.Value);
+                    break;
+                case ShapeType.Area:
+                    collidedObjects = Physics2D.OverlapAreaAll(pos + pointA.Value, pos + pointB.Value, layerMask.Value, minDepth.Value, maxDepth.Value);
+                    break;
+                case ShapeType.Capsule:
+                    collidedObjects = Physics2D.OverlapCapsuleAll(pos + offset.Value, size.Value, direction , 0, layerMask.Value, minDepth.Value, maxDepth.Value);
+                    break;
+                case ShapeType.Point:
+                    collidedObjects = Physics2D.OverlapPointAll(pos + point.Value, layerMask.Value, minDepth.Value, maxDepth.Value);
+                    break;
+                default:
+                    break;
+            }
+
+            if (collidedObjects.Length > 0)
+            {
+                storeDidHit.Value = true;
+                storeGameObjects.Values = new GameObject[collidedObjects.Length];
+                for (int i = 0; i < storeGameObjects.Values.Length; i++)
+                {
+                    storeGameObjects.Values[i] = collidedObjects[i].gameObject;
+                }
+            }
+            else
+            {
+                storeDidHit.Value = false;
+            }
+
+        }
+    }
+}
+
