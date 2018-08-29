52189e1adda2f87cc3ed77d18f4f1dd8f243258f
Jean Fabre <contact@fabrejean.net>
2018-08-29 11:27:17 +0300

    New actions for randomXXX using seeds

---
+++ Assets/PlayMaker Custom Actions/Math/RandomArrayVector3Seed.cs
@@ -0,0 +1,55 @@
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+// http://hutonggames.com/playmakerforum/index.php?topic=19190.msg83571#msg83571
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Math)]
+	[Tooltip("Sets a Float Variable to a random value between Min/Max.")]
+	public class RandomArrayVector3Seed: FsmStateAction
+	{    
+        private FsmInt index;
+        [RequiredField]
+		public FsmInt maxindex;
+		[RequiredField]
+		public FsmInt seed;
+		[RequiredField]
+		public FsmFloat min;
+		[RequiredField]
+		public FsmFloat max;
+		[RequiredField]
+		public FsmVector3 vector3Result;
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The Array Variable to use.")]
+        public FsmArray arrayResult;
+
+		public override void Reset()
+		{
+			min = 0f;
+			max = 1f;
+            seed = 0;
+            maxindex = 0;
+			arrayResult = null;
+		}
+
+		public override void OnEnter()
+		{
+            
+	  Random.InitState(seed.Value);
+      
+        for (int i = 0; i < maxindex.Value; i++)
+        { 
+            arrayResult.Resize(arrayResult.Length + 1);
+            vector3Result.Value = new Vector3(Random.Range(min.Value, max.Value), Random.Range(min.Value, max.Value), Random.Range(min.Value, max.Value));
+            arrayResult.Set(arrayResult.Length - 1,vector3Result.Value);
+			
+			Finish();
+		}
+	}
+}
+}
+
---
+++ Assets/PlayMaker Custom Actions/Math/RandomIntSeed.cs
@@ -0,0 +1,41 @@
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+// http://hutonggames.com/playmakerforum/index.php?topic=19190.msg83571#msg83571
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Math)]
+	[Tooltip("Sets a Float Variable to a random value between Min/Max.")]
+	public class RandomIntSeed : FsmStateAction
+	{
+        
+		[RequiredField]
+		public FsmInt seed;
+		[RequiredField]
+		public FsmInt min;
+		[RequiredField]
+		public FsmInt max;
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmInt storeResult;
+
+		public override void Reset()
+		{
+			min = 0;
+			max = 1;
+            
+			storeResult = null;
+		}
+
+		public override void OnEnter()
+		{
+            
+			Random.InitState(seed.Value);
+            storeResult.Value = Random.Range(min.Value, max.Value);
+			Finish();
+		}
+	}
+}
