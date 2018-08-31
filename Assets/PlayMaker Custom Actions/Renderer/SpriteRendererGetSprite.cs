15d49819caad6b7686721a79d55badfca963a96b
Jean Fabre <contact@fabrejean.net>
2018-08-31 17:04:01 +0300

    New Action SpriteRendererGetSprite

---
+++ Assets/PlayMaker Custom Actions/Renderer/SpriteRendererGetSprite.cs
@@ -0,0 +1,52 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Renderer)]
+	[Tooltip("Gets the image sprite of a SpriteRenderer component.")]
+	public class SpriteRendererGetSprite : ComponentAction<SpriteRenderer>
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(SpriteRenderer))]
+		[Tooltip("The GameObject with the SpriteRenderer component.")]
+		public FsmOwnerDefault gameObject;
+
+		[RequiredField]
+		[Tooltip("The source sprite of the SpriteRenderer component.")]
+		[UIHint(UIHint.Variable)]
+		[ObjectType(typeof(Sprite))]
+		public FsmObject sprite;
+
+		private SpriteRenderer sp;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			sprite = null;
+		}
+		
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (UpdateCache(go))
+			{
+				sp = cachedComponent;
+			}
+			
+			DoSetImageSourceValue();
+
+			Finish();
+		}
+
+	    private void DoSetImageSourceValue()
+		{
+			if (sp!=null)
+			{
+				sprite.Value = sp.sprite;
+			}
+		}
+	}
+}
