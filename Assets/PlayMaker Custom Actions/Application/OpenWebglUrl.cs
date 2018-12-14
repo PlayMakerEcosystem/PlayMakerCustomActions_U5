e4761e5d44bfa021e64164954905e0b7fe548acd
Jean Fabre <contact@fabrejean.net>
2018-12-14 11:59:55 +0300

    New action to open new tab for webgl builds

---
+++ Assets/PlayMaker Custom Actions/Application/OpenWebglUrl.cs
@@ -0,0 +1,44 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__
+EcoMetaStart
+{
+"script dependancies":[
+                        "Assets/PlayMaker Custom Plugins/Plugins/WebGL/PlayMakerOpenWindow"
+                      ]
+}
+EcoMetaEnd
+---*/
+
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Application)]
+    [Tooltip("Opens a new browser window for a given url, works only in Webgl")]
+    public class OpenWebglWindow : FsmStateAction
+    {
+
+        [Tooltip("The Url to open")]
+        public FsmString url;
+
+        public override void Reset()
+        {
+            url = null;
+        }
+
+        public override void OnEnter()
+        {
+#if !UNITY_EDITOR && UNITY_WEBGL
+            openWindow(url.Value);
+#else
+            Application.OpenURL(url.Value);
+#endif
+        }
+
+#if UNITY_WEBGL
+        [DllImport("__Internal")]
+        private static extern void openWindow(string url);
+#endif
+    }
+}
