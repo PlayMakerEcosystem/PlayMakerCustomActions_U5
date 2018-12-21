924b3b0165f8a5733b99da0674ca00dc8474ed80
Jean Fabre <contact@fabrejean.net>
2018-12-21 09:19:37 +0300

    New action FindGameObjectOfType

---
+++ Assets/PlayMaker Custom Actions/GameObject/FindGameObjectByType.cs
@@ -0,0 +1,62 @@
+// (c) Copyright HutongGames, LLC 2010-2018. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using System;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Finds a Game Object by type")]
+	public class FindGameObjectByType : FsmStateAction
+	{
+
+        [Tooltip("The Type. Should be a component type.")]
+        public string type;
+
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the result in a GameObject variable.")]
+		public FsmGameObject store;
+
+        [Tooltip("Event sent if this type was found in the hierarchy")]
+        public FsmEvent found;
+
+        [Tooltip("Event sent if this type was not found in the hierarchy")]
+        public FsmEvent notFound;
+
+		public override void Reset()
+		{
+            type = "UnityEngine.Camera, UnityEngine";
+            store = null;
+            found = null;
+            notFound = null;
+		}
+
+		public override void OnEnter()
+		{
+			Find();
+			Finish();
+		}
+
+		void Find()
+		{
+            Type _type = Type.GetType(type);
+
+            var _result =  UnityEngine.Object.FindObjectOfType(_type);
+            Component _c = _result as Component;
+
+            if (_c != null)
+            {
+                store.Value = _c.gameObject;
+                Fsm.Event(found);
+            }
+            else{
+                Fsm.Event(notFound);
+            }
+
+		}
+
+
+	}
+}
