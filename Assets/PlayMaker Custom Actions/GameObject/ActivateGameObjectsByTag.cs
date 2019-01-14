e239c6a6ede78a40d0f695c1190ffd4541f3e400
Jean Fabre <contact@fabrejean.net>
2019-01-14 10:12:42 +0300

    Fixed ActivateGameObjectsByTag

--- Assets/PlayMaker Custom Actions/GameObject/ActivateGameObjectsByTag.cs
+++ Assets/PlayMaker Custom Actions/GameObject/ActivateGameObjectsByTag.cs
@@ -1,13 +1,16 @@
 // License: Attribution 4.0 International (CC BY 4.0)
 //Original Action : https://hutonggames.com/playmakerforum/index.php?topic=19971.0
 /*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
 
 namespace HutongGames.PlayMaker.Actions
 {
 	[ActionCategory(ActionCategory.GameObject)]
 	[Tooltip("Activates/deactives all Game Object with a given tag.")]
-	public class ActivateObjectsByTag : FsmStateAction
+	public class ActivateGameObjectsByTag : FsmStateAction
 	{
 		[RequiredField]
 		[UIHint(UIHint.Tag)]
@@ -27,12 +30,35 @@ namespace HutongGames.PlayMaker.Actions
 
 		public override void OnEnter()
 		{
-			 gos = GameObject.FindGameObjectsWithTag(tag.Value);
-			
-	        foreach (GameObject go in gos) 
-			{
-				go.SetActive(activate.Value);
-	        }
+            if (activate.Value == false)
+            {
+                gos = GameObject.FindGameObjectsWithTag(tag.Value);
+
+                foreach (GameObject go in gos)
+                {
+                    go.SetActive(false);
+                }
+            }
+            else
+            {
+                foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+                {
+                    if (go.hideFlags != HideFlags.None)
+                        continue;
+
+#if UNITY_EDITOR
+                    if (PrefabUtility.GetPrefabType(go) == PrefabType.Prefab || PrefabUtility.GetPrefabType(go) == PrefabType.ModelPrefab)
+                        continue;
+#endif
+
+                    if (go.tag == tag.Value)
+                    {
+                        go.SetActive(true);
+                    }
+                }
+            }
+
+          
 
 			Finish();
 		}
