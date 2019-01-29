e2446ce5b1e43f780e974d03c3b07bf77a4ba843
Jean Fabre <contact@fabrejean.net>
2019-01-29 11:51:49 +0300

    New action GetSqrMagnitude

---
+++ Assets/PlayMaker Custom Actions/GameObject/GetSqrMagnitude.cs
@@ -0,0 +1,62 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Measures the Square Distance between 2 Game Objects and stores the result in a Float Variable. It's faster than distance, so for relative comparision, it's preferable")]
+	public class GetSqrMagnitude : FsmStateAction
+	{
+		[RequiredField]
+        [Tooltip("Measure distance from this GameObject.")]
+		public FsmOwnerDefault gameObject;
+		
+        [RequiredField]
+		[Tooltip("Target GameObject.")]
+        public FsmGameObject target;
+		
+        [RequiredField]
+		[UIHint(UIHint.Variable)]
+        [Tooltip("Store the square distance in a float variable.")]
+		public FsmFloat storeResult;
+		
+        [Tooltip("Repeat every frame.")]
+        public bool everyFrame;
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			target = null;
+			storeResult = null;
+			everyFrame = true;
+		}
+		
+		public override void OnEnter()
+		{
+			DoGetDistance();
+
+		    if (!everyFrame)
+		    {
+		        Finish();
+		    }
+		}
+		public override void OnUpdate()
+		{
+			DoGetDistance();
+		}		
+		
+		void DoGetDistance()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+		    if (go == null || target.Value == null || storeResult == null)
+		    {
+		        return;
+		    }
+					
+			storeResult.Value = Vector3.SqrMagnitude(go.transform.position-target.Value.transform.position);
+		}
+
+	}
+}
