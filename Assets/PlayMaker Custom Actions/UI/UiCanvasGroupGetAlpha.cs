01d3fb3b0009fdac04a19dd3655069ef2202f691
Jean Fabre <contact@fabrejean.net>
2019-03-18 10:07:09 +0300

    new action UiCanvasGroupGetAlpha

---
+++ Assets/PlayMaker Custom Actions/UI/UiCanvasGroupGetAlpha.cs
@@ -0,0 +1,60 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.UI)]
+    [Tooltip("Get CanvasGroup Alpha.")]
+    public class UiCanvasGroupGetAlpha : ComponentAction<CanvasGroup>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(CanvasGroup))]
+        [Tooltip("The GameObject with a UI CanvasGroup component.")]
+        public FsmOwnerDefault gameObject;
+
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The alpha of the UI CanvasGroup.")]
+        public FsmFloat alpha;
+
+        [Tooltip("Repeats every frame, useful for animation")]
+        public bool everyFrame;
+
+        public override void Reset()
+        {
+            gameObject = null;
+            alpha = null;
+
+            everyFrame = false;
+        }
+
+        public override void OnEnter()
+        {
+           
+            DoGetValue();
+
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            DoGetValue();
+        }
+
+        private void DoGetValue()
+        {
+            if (UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+                alpha.Value = this.cachedComponent.alpha;
+            }
+        }
+
+
+
+    }
+}
