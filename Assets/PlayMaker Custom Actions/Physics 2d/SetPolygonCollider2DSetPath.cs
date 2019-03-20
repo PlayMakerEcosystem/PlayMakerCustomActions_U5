1417babe361e259577a2b4fe1130a9615d4de8a1
Jean Fabre <contact@fabrejean.net>
2019-03-20 10:23:07 +0300

    New Actions to set path for polygonCollider2D components

---
+++ Assets/PlayMaker Custom Actions/Physics 2d/SetPolygonCollider2DSetPath.cs
@@ -0,0 +1,48 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Physics)]
+    [Tooltip("Sets the path defining the polygon for a PolygonCollider2D")]
+    public class SetPolygonCollider2DSetPath : ComponentAction<PolygonCollider2D>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(PolygonCollider2D))]
+        public FsmOwnerDefault gameObject;
+
+        public FsmInt pathIndex;
+
+        public FsmVector2[] path;
+
+        Vector2[] _array;
+
+        public override void Reset()
+        {
+            gameObject = null;
+            path = null;
+            pathIndex = null; 
+        }
+
+        public override void OnEnter()
+        {
+            if (this.UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+                _array = new Vector2[path.Length];
+
+                int i = 0;
+                foreach (FsmVector2 _v in path)
+                {
+                    _array[i] = path[i].Value;
+                    i++;
+                }
+
+                this.cachedComponent.SetPath(pathIndex.Value, _array);
+                
+            }
+            Finish();
+        }
+    }
+}
---
+++ Assets/PlayMaker Custom Actions/Physics 2d/SetPolygonCollider2DSetPathCount.cs
@@ -0,0 +1,38 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Physics)]
+    [Tooltip("Sets the number of path path defining the polygon for a PolygonCollider2D")]
+    public class SetPolygonCollider2DSetPathCount : ComponentAction<PolygonCollider2D>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(PolygonCollider2D))]
+        public FsmOwnerDefault gameObject;
+
+        public FsmInt pathCount;
+
+
+        public override void Reset()
+        {
+            gameObject = null;
+            pathCount = null;
+  
+        }
+
+        public override void OnEnter()
+        {
+            if (this.UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+
+                this.cachedComponent.pathCount = pathCount.Value;
+
+
+            }
+            Finish();
+        }
+    }
+}
