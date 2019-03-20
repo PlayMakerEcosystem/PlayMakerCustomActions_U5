bb96a9ddb6533c676445f10f9c217fb19e34aba5
Jean Fabre <contact@fabrejean.net>
2019-03-20 09:24:40 +0300

    New actions to set and get fixedJoint2d properties

---
+++ Assets/PlayMaker Custom Actions/Physics 2d/GetFixedJoint2DProperties.cs
@@ -0,0 +1,135 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Physics)]
+    [Tooltip("Gets the various properties of a FixedJoint2D component.")]
+    public class GetFixedJoint2DProperties : ComponentAction<FixedJoint2D>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(FixedJoint2D))]
+        public FsmOwnerDefault gameObject;
+
+        [UIHint(UIHint.Variable)]
+        public FsmBool enableCollision;
+
+        [UIHint(UIHint.Variable)]
+        public FsmGameObject connectedBody;
+
+        [UIHint(UIHint.Variable)]
+        public FsmBool autoConfigureConnectedAnchor;
+
+        [UIHint(UIHint.Variable)]
+        public FsmVector2 anchor;
+
+        [UIHint(UIHint.Variable)]
+        public FsmVector2 connectedAnchor;
+
+        [UIHint(UIHint.Variable)]
+        public FsmFloat dampingRatio;
+
+        [UIHint(UIHint.Variable)]
+        public FsmFloat frequency;
+
+        [UIHint(UIHint.Variable)]
+        public FsmFloat breakForce;
+
+        [UIHint(UIHint.Variable)]
+        public FsmFloat breakTorque;
+
+        [UIHint(UIHint.Variable)]
+        public FsmVector2 reactionForce;
+
+        public bool everyFrame;
+
+        public override void Reset()
+        {
+            gameObject = null;
+            enableCollision = null;
+            anchor = null;
+            connectedAnchor = null;
+            reactionForce = null;
+            autoConfigureConnectedAnchor = null;
+            breakForce = null;
+            breakTorque = null;
+            dampingRatio = null;
+            frequency = null;
+            connectedBody = null;
+
+        }
+        public override void OnEnter()
+        {
+            ExecuteAction();
+
+            if (!everyFrame)
+            {
+                Finish();
+            }
+
+        }
+        public override void OnUpdate()
+        {
+            ExecuteAction();
+        }
+
+
+        void ExecuteAction()
+        {
+            if (this.UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+                if (!anchor.IsNone)
+                {
+                    anchor.Value = this.cachedComponent.anchor;
+                }
+
+                if (!connectedAnchor.IsNone)
+                {
+                    connectedAnchor.Value = this.cachedComponent.connectedAnchor;
+                }
+
+                if (!reactionForce.IsNone)
+                {
+                    reactionForce.Value = this.cachedComponent.reactionForce;
+                }
+
+                if (!autoConfigureConnectedAnchor.IsNone)
+                {
+                    autoConfigureConnectedAnchor.Value = this.cachedComponent.autoConfigureConnectedAnchor;
+                }
+
+                if (!enableCollision.IsNone)
+                {
+                    enableCollision.Value = this.cachedComponent.enableCollision;
+                }
+
+                if (!breakForce.IsNone)
+                {
+                    breakForce.Value = this.cachedComponent.breakForce;
+                }
+
+                if (!breakTorque.IsNone)
+                {
+                    breakTorque.Value = this.cachedComponent.breakTorque;
+                }
+
+                if (!dampingRatio.IsNone)
+                {
+                    dampingRatio.Value = this.cachedComponent.dampingRatio;
+                }
+
+                if (!frequency.IsNone)
+                {
+                    frequency.Value = this.cachedComponent.frequency;
+                }
+
+                if (!connectedBody.IsNone)
+                {
+                    connectedBody.Value = this.cachedComponent.connectedBody!=null?this.cachedComponent.connectedBody.gameObject:null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PlayMaker Custom Actions/Physics 2d/SetFixedJoint2DProperties.cs
@@ -0,0 +1,125 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Physics)]
+    [Tooltip("Sets the various properties of a FixedJoint2D component.")]
+    public class SetFixedJoint2DProperties : ComponentAction<FixedJoint2D>
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(FixedJoint2D))]
+        public FsmOwnerDefault gameObject;
+
+        public FsmBool enableCollision;
+
+        public FsmGameObject connectedBody;
+
+      
+        public FsmBool autoConfigureConnectedAnchor;
+
+       
+        public FsmVector2 anchor;
+
+       
+        public FsmVector2 connectedAnchor;
+
+
+        public FsmFloat dampingRatio;
+
+       
+        public FsmFloat frequency;
+
+
+        public FsmFloat breakForce;
+
+
+        public FsmFloat breakTorque;
+
+
+        public bool everyFrame;
+
+        public override void Reset()
+        {
+            gameObject = null;
+            enableCollision = new FsmBool() { UseVariable = true };
+            anchor = new FsmVector2() { UseVariable = true };
+            connectedAnchor = new FsmVector2() { UseVariable = true };
+            autoConfigureConnectedAnchor = new FsmBool() { UseVariable = true };
+            breakForce = new FsmFloat() { UseVariable = true };
+            breakTorque = new FsmFloat() { UseVariable = true };
+            dampingRatio = new FsmFloat() { UseVariable = true };
+            frequency = new FsmFloat() { UseVariable = true };
+            connectedBody = new FsmGameObject() { UseVariable = true };
+
+        }
+        public override void OnEnter()
+        {
+            ExecuteAction();
+
+            if (!everyFrame)
+            {
+                Finish();
+            }
+
+        }
+        public override void OnUpdate()
+        {
+            ExecuteAction();
+        }
+
+
+        void ExecuteAction()
+        {
+            if (this.UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+                if (!anchor.IsNone)
+                {
+                    this.cachedComponent.anchor = anchor.Value;
+                }
+
+                if (!connectedAnchor.IsNone)
+                {
+                    this.cachedComponent.connectedAnchor = connectedAnchor.Value;
+                }
+
+                if (!autoConfigureConnectedAnchor.IsNone)
+                {
+                    this.cachedComponent.autoConfigureConnectedAnchor = autoConfigureConnectedAnchor.Value;
+                }
+
+                if (!enableCollision.IsNone)
+                {
+                    this.cachedComponent.enableCollision = enableCollision.Value;
+                }
+
+                if (!breakForce.IsNone)
+                {
+                    this.cachedComponent.breakForce = breakForce.Value;
+                }
+
+                if (!breakTorque.IsNone)
+                {
+                   this.cachedComponent.breakTorque = breakTorque.Value;
+                }
+
+                if (!dampingRatio.IsNone)
+                {
+                    this.cachedComponent.dampingRatio = dampingRatio.Value;
+                }
+
+                if (!frequency.IsNone)
+                {
+                    this.cachedComponent.frequency = frequency.Value;
+                }
+
+                if (!connectedBody.IsNone)
+                {
+                    this.cachedComponent.connectedBody =  connectedBody.Value!=null? connectedBody.Value.GetComponent<Rigidbody2D>(): null;
+                }
+            }
+        }
+    }
+}
