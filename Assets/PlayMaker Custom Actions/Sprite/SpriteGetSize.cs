c4f0261a10f34dea341b92ebb8c94ce51e63a586
Jean Fabre <contact@fabrejean.net>
2019-03-27 13:02:00 +0300

    New actions to set and get sprite sizes

---
+++ Assets/PlayMaker Custom Actions/Sprite/SpriteGetSize.cs
@@ -0,0 +1,90 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("sprite")]
+	[Tooltip("Gets a Sprite size. Object must have a Sprite Renderer.")]
+	public class SpriteGetSize : ComponentAction<SpriteRenderer>
+	{
+
+		[RequiredField]
+		[Tooltip("The SpriteRenderer to control.")]
+		[CheckForComponent(typeof(SpriteRenderer))]
+		public FsmOwnerDefault gameObject;
+
+        [Tooltip("The size")]
+        [UIHint(UIHint.Variable)]
+        public FsmVector2 size;
+
+		[Tooltip("The size X value")]
+        [UIHint(UIHint.Variable)]
+        public FsmFloat sizeX;
+
+		[Tooltip("The size Y value")]
+        [UIHint(UIHint.Variable)]
+        public FsmFloat sizeY;
+
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+
+		public override void Reset()
+		{
+			gameObject = null;
+            sizeX = null;
+            sizeY = null;
+            size = null;
+
+		}
+
+		public override void OnEnter()
+		{
+            Execute();
+
+            
+			if (!everyFrame) 
+			{
+				Finish ();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+		
+                Execute();
+			
+		}
+
+		void Execute()
+		{
+            if (!UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+                return;
+            }
+
+
+            if (!size.IsNone)
+            {
+                size.Value = this.cachedComponent.size;
+            }
+
+            if (!sizeX.IsNone)
+            {
+                sizeX.Value = this.cachedComponent.size.x;
+            }
+
+            if (!sizeY.IsNone)
+            {
+                sizeY.Value = this.cachedComponent.size.y;
+            }
+
+		}
+
+
+
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/Sprite/SpriteSetSize.cs
@@ -0,0 +1,114 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("sprite")]
+	[Tooltip("Sets a Sprite size. Object must have a Sprite Renderer.")]
+	public class SpriteSetSize : ComponentAction<SpriteRenderer>
+	{
+
+		[RequiredField]
+		[Tooltip("The SpriteRenderer to control.")]
+		[CheckForComponent(typeof(SpriteRenderer))]
+		public FsmOwnerDefault gameObject;
+
+        [Tooltip("The size")]
+        public FsmVector2 size;
+
+		[Tooltip("The size X value")]
+		public FsmFloat sizeX;
+
+		[Tooltip("The size Y value")]
+		public FsmFloat sizeY;
+
+		[Tooltip("Reset flip values when state exits")]
+		public FsmBool resetOnExit;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+
+		Vector2 size_orig;
+
+        Vector2 _size = new Vector2();
+		public override void Reset()
+		{
+			gameObject = null;
+			sizeX = new FsmFloat() { UseVariable = true };
+			sizeY = new FsmFloat() { UseVariable = true };
+            size = null;
+			resetOnExit = false;
+
+		}
+
+		public override void OnEnter()
+		{
+            if (UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+
+                if (resetOnExit.Value)
+                {
+                    size_orig = this.cachedComponent.size;
+                }
+
+                Execute();
+
+            }
+			if (!everyFrame) 
+			{
+				Finish ();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+		
+                Execute();
+			
+		}
+
+
+		public override void OnExit()
+		{
+			if (resetOnExit.Value)
+			{
+                this.cachedComponent.size = size_orig;
+			}
+		}
+
+
+		void Execute()
+		{
+            if (!UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject)))
+            {
+                return;
+            }
+
+            _size = this.cachedComponent.size;
+
+            if (!size.IsNone)
+            {
+                _size = size.Value;
+            }
+
+            if (!sizeX.IsNone)
+            {
+                _size.x = sizeX.Value;
+            }
+
+            if (!sizeY.IsNone)
+            {
+                _size.y = sizeY.Value;
+            }
+
+            this.cachedComponent.size = _size;
+            
+		}
+
+
+
+	}
+}
