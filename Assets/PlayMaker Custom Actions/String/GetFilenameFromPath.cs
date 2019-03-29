011826bb6ed72dce634861b06e5ef15f888f9c39
Jean Fabre <contact@fabrejean.net>
2019-03-29 11:15:40 +0300

    New Action GetFilenameFromPath

---
+++ Assets/PlayMaker Custom Actions/String/GetFilenameFromPath.cs
@@ -0,0 +1,58 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using System;
+using System.Text;
+using System.IO;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.String)]
+    [Tooltip("Get the filename from a path string")]
+    public class GetFilenameFromPath : FsmStateAction
+    {
+        [RequiredField]
+        public FsmString path;
+
+        [UIHint(UIHint.Variable)]
+        public FsmString filename;
+
+        [UIHint(UIHint.Variable)]
+        public FsmString name;
+        [UIHint(UIHint.Variable)]
+        public FsmString extension;
+
+        public override void Reset()
+        {
+            path = null;
+            filename = null;
+
+        }
+
+        public override void OnEnter()
+        {
+
+            string _path = Path.Combine("", path.Value);
+
+            if (!filename.IsNone)
+            {
+                filename.Value = Path.GetFileName(_path);
+            }
+
+            if (!name.IsNone)
+            {
+                name.Value = Path.GetFileNameWithoutExtension(_path);
+            }
+
+            if (!extension.IsNone)
+            {
+                extension.Value = Path.GetExtension(_path);
+            }
+
+            Finish();
+        }
+
+      
+    }
+}
