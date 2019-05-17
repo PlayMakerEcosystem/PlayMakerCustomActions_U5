af377ac50d50dd9bddd19d38f2b2a53f89f49528
Jean Fabre <contact@fabrejean.net>
2019-05-17 08:30:30 +0300

    New action GetParent2

---
+++ Assets/PlayMaker Custom Actions/GameObject/GetParent2.cs
@@ -0,0 +1,47 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.GameObject)]
+	[Tooltip("Gets the Parent of a Game Object with option to get it everyframe for complex one state flow")]
+	public class GetParent2 : FsmStateAction
+	{
+		[RequiredField]
+		public FsmOwnerDefault gameObject;
+		[UIHint(UIHint.Variable)]
+		public FsmGameObject storeResult;
+
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			gameObject = null;
+			storeResult = null;
+		}
+
+		public override void OnEnter()
+		{
+			Execute();
+
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			Execute();
+		}
+
+		public void Execute()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go != null)
+				storeResult.Value = go.transform.parent == null ? null : go.transform.parent.gameObject;
+			else
+				storeResult.Value = null;
+		}
+	}
+}
