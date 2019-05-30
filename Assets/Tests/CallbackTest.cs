21a8f9cc08812a846dcf95272bc999bfcb772b75
Jean Fabre <contact@fabrejean.net>
2019-05-30 12:06:41 +0300

    Moved U4 actions ( wheels and OpenUrl)

---
+++ Assets/Tests/CallbackTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CallbackTest : MonoBehaviour
+{
+
+	public static CallbackTest Instance;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TriggerSomething(UnityAction<string> doneCallback)
+	{
+		Debug.Log("TriggerSomething");
+		_callbackAction = doneCallback;
+		
+		Invoke("Callback",3f);
+	}
+
+	private UnityAction<string> _callbackAction;
+	
+	void Callback()
+	{
+		Debug.Log("Callback");
+		_callbackAction.Invoke("hello");
+	}
+	
+}
---
+++ Assets/Tests/CallbackTestAction.cs
@@ -0,0 +1,23 @@
+﻿
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using System.Collections;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	public class CallbackTestAction : FsmStateAction
+	{
+
+
+		public override void OnEnter()
+		{
+			CallbackTest.Instance.TriggerSomething(MyCallback);
+		}
+
+		void MyCallback(string value)
+		{
+			UnityEngine.Debug.Log("CallbackTestAction got called back! : " +value);
+		}
+	}
+}
