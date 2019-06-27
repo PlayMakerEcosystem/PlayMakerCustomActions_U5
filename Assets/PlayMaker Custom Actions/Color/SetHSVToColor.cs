7dcfb88e21c903873d044e41a13beee8ccfa1ce9
DjayDino <jinxtergames@gmail.com>
2019-06-27 17:31:50 +0200

    Added 2 New Actions : String Insert At Index and Set HSV To Color

---
+++ Assets/PlayMaker Custom Actions/Color/SetHSVToColor.cs
@@ -0,0 +1,69 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// Made by djaydino -- http://www.jinxtergames.com/ --
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Color)]
+	[Tooltip("Creates colour from HSV input.")]
+	public class SetHSVToColor : FsmStateAction
+	{
+
+        [RequiredField]
+        [HasFloatSlider(0,1)]
+		public FsmFloat hue;
+
+        [RequiredField]
+        [HasFloatSlider(0,1)]
+		public FsmFloat saturation;
+
+        [RequiredField]
+        [HasFloatSlider(0,1)]
+		public FsmFloat brightness;
+
+        [RequiredField]
+        [Tooltip("Output HDR colours. If true, the returned colour will not be clamped to [0..1].")]
+        public FsmBool hdr;
+
+        [ActionSection("Result")]
+
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        public FsmColor colorVariable;
+
+
+		
+		public bool everyFrame;
+
+		public override void Reset()
+		{
+			colorVariable = null;
+            hue = 0;
+			saturation = 0;
+			brightness = 0;
+            hdr = false;
+
+            everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+            DoSetHSVToRGBA();
+			
+			if (!everyFrame)
+				Finish();		
+		}
+
+		public override void OnUpdate()
+		{
+            DoSetHSVToRGBA();
+		}
+
+		void DoSetHSVToRGBA()
+		{
+            colorVariable.Value = Color.HSVToRGB(hue.Value, saturation.Value, brightness.Value, hdr.Value);
+		}
+	}
+}
---
+++ Assets/PlayMaker Custom Actions/String/StringInsertAtIndex.cs
@@ -0,0 +1,52 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// Made by Djaydino http://www.jinxtergames.com/
+
+using UnityEngine;
+using System;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.String)]
+    [Tooltip("add a character into the string at index nr")]
+    public class StringInsertAtIndex : FsmStateAction
+    {
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        public FsmString stringVariable;
+
+        [Tooltip("The character to insert")]
+        public FsmString character;
+
+        [Tooltip("Index to place the character")]
+        public FsmInt index;
+
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [Tooltip("The string result with the character included")]
+        public FsmString stringResult;
+
+        public FsmEvent error;
+
+        public override void Reset()
+        {
+            stringVariable = null;
+            index = null;
+            character = null;
+            stringResult = null;
+        }
+
+        public override void OnEnter()
+        {
+            if (stringVariable.Value.Length >= index.Value)
+            {
+                stringResult.Value = stringVariable.Value.Insert(index.Value, character.Value);
+            }
+            else
+            {
+                Fsm.Event(error);
+            }
+            Finish();
+        }
+    }
+}
