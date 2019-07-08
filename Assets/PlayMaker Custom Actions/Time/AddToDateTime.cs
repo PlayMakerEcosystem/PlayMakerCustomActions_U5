ab3e23784dc0707ed1a646cdf3fec1bf1ac2039a
Jean Fabre <contact@fabrejean.net>
2019-07-08 14:16:15 +0200

    added new action AddToDateTime

---
+++ Assets/PlayMaker Custom Actions/Time/AddToDateTime.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Time)]
+	[Tooltip("Adds seconds to date and time and stores it in a string variable. An optional format string gives you a lot of control over the formatting (see online docs for format syntax).")]
+	public class AddToDateTime : FsmStateAction
+	{
+	
+        [RequiredField]
+        [Tooltip("The time and date to add seconds to, following the format described in 'dateFormat'")]
+        public FsmString startDate;
+
+		[Tooltip("format string. E.g., MM/dd/yyyy HH:mm:ss")]
+		public FsmString dateFormat;
+		
+        [RequiredField]
+        [Tooltip("Integer seconds to add to date.")]
+        public FsmInt add;
+		
+		[ActionSection("Result")]
+        [UIHint(UIHint.Variable)]
+		[Tooltip("Store result date and time as a string.")]
+		public FsmString storeString;
+		
+        
+
+		CultureInfo provider = CultureInfo.InvariantCulture;
+		
+		public override void Reset()
+		{
+            startDate = null;
+            storeString = null;
+            dateFormat = "MM/dd/yyyy HH:mm:ss";
+            add = null;
+		}
+
+		public override void OnEnter()
+		{
+            
+            DateTime _startDate = DateTime.ParseExact(startDate.Value, dateFormat.Value, provider);
+
+            //this works
+           // _startDate = _startDate.Add(System.TimeSpan.FromSeconds(100));       
+
+
+            //but if I try to add the int variable like so
+            _startDate = _startDate.Add(System.TimeSpan.FromSeconds((int)add.Value));
+
+            // then it gives an error:
+            //  Assets / PlayMaker / Actions / Time / AddToDateTime.cs(41, 69): error CS1503: Argument `#1' cannot convert `HutongGames.PlayMaker.FsmInt' expression to type `double'
+            // how do I fix this?
+
+            storeString.Value = _startDate.ToString(dateFormat.Value);
+      	}
+
+#if UNITY_EDITOR
+	    public override string AutoName()
+	    {
+            return ActionHelpers.AutoName(this, startDate, add, storeString, dateFormat);
+	    }
+#endif
+
+	}
+}
