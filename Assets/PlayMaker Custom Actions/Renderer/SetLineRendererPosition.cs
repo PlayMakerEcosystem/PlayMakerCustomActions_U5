a529142aa41e089dbb63716a638a5aa88091dc52
Jean Fabre <contact@fabrejean.net>
2019-10-15 16:35:56 +0300

    Moved renderer actions from U4

---
+++ Assets/PlayMaker Custom Actions/Renderer/SetLineRendererPosition.cs
@@ -0,0 +1,126 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__
+EcoMetaStart
+{
+"script dependancies":[
+						"Assets/PlayMaker Custom Actions/__Internal/PlayMakerActionsUtils.cs"
+					],
+"version":"1.1.0"
+}
+EcoMetaEnd
+---*/
+// Keywords: line renderer
+
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Renderer)]
+	[Tooltip("Set a particular position of a lineRenderer")]
+	public class SetLineRendererPosition : FsmStateAction
+	{
+
+		[ActionSection("Setup")]
+		[RequiredField]
+		[Tooltip("The GameObject with the LineRenderer component.")]
+		[CheckForComponent(typeof(LineRenderer))]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("The index")]
+		[RequiredField]
+		public FsmInt index;
+
+		[Tooltip("The Position")]
+		[RequiredField]
+		public FsmVector3 position;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+		public PlayMakerActionsUtils.EveryFrameUpdateSelector updateType;
+
+		LineRenderer _lr;
+
+		public override void Reset ()
+		{
+			gameObject = null;
+			position = null;
+			everyFrame = false;
+			
+			updateType = PlayMakerActionsUtils.EveryFrameUpdateSelector.OnUpdate;
+		}
+
+		public override void OnPreprocess()
+		{
+			if (updateType == PlayMakerActionsUtils.EveryFrameUpdateSelector.OnFixedUpdate)
+			{
+				Fsm.HandleFixedUpdate = true;
+			}
+			
+#if PLAYMAKER_1_8_5_OR_NEWER
+			if (updateType == PlayMakerActionsUtils.EveryFrameUpdateSelector.OnLateUpdate)
+			{
+				Fsm.HandleLateUpdate = true;
+			}
+#endif
+		}
+		
+		public override void OnUpdate()
+		{
+			if (updateType == PlayMakerActionsUtils.EveryFrameUpdateSelector.OnUpdate)
+			{
+				Execute();
+			}
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		public override void OnLateUpdate()
+		{
+			if (updateType == PlayMakerActionsUtils.EveryFrameUpdateSelector.OnLateUpdate)
+			{
+				Execute();
+			}
+
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+		
+		public override void OnFixedUpdate()
+		{
+			if (updateType == PlayMakerActionsUtils.EveryFrameUpdateSelector.OnFixedUpdate)
+			{
+				Execute();
+			}
+
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		void Execute()
+		{
+			var go = Fsm.GetOwnerDefaultTarget (gameObject);
+			if (go == null) {
+				return;
+			}
+			
+			_lr = go.GetComponent<LineRenderer>();
+
+			if (_lr == null)
+			{
+				return;
+			}
+
+			_lr.SetPosition(index.Value,position.Value);
+
+		}
+	}
+}
