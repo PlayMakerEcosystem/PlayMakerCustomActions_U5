d7431a0b72d6ab0f02aa51375b44f06e8cce8951
Jean Fabre <contact@fabrejean.net>
2019-12-04 09:04:16 +0300

    New action FloatAbsAdvanced

---
+++ Assets/PlayMaker Custom Actions/Math/FloatAbsAdvanced.cs
@@ -0,0 +1,57 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__
+EcoMetaStart
+{
+"script dependancies":[
+						"Assets/PlayMaker Custom Actions/__Internal/FsmStateActionAdvanced.cs"
+					]
+}
+EcoMetaEnd
+---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Math)]
+	[Tooltip("Sets a Float variable to its absolute value.")]
+	public class FloatAbsAdvanced : FsmStateActionAdvanced
+	{
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+        [Tooltip("The Float variable.")]
+		public FsmFloat floatVariable;
+
+		[Tooltip("The multiplier. Set to -1 to force the abs result to be negative for example")]
+		public FsmFloat multiplier;
+		
+
+		public override void Reset()
+		{
+			base.Reset();
+			floatVariable = null;
+			multiplier = new FsmFloat(){UseVariable = true};
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			DoFloatAbs();
+			
+			if (!everyFrame)
+			{
+			    Finish();
+			}
+		}
+
+		public override void OnActionUpdate()
+		{
+			DoFloatAbs();
+		}
+		
+		void DoFloatAbs()
+		{
+			floatVariable.Value = Mathf.Abs(floatVariable.Value) * (multiplier.IsNone?1f:multiplier.Value);
+		}
+	}
+}
