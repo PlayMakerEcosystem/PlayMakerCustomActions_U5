4f5323fce90b192e705f1bfc48758f188e008e0b
Jean Fabre <contact@fabrejean.net>
2019-12-16 10:43:24 +0300

    updated SetVSyncCount

--- Assets/PlayMaker Custom Actions/QualitySettings/SetVSyncCount.cs
+++ Assets/PlayMaker Custom Actions/QualitySettings/SetVSyncCount.cs
@@ -3,20 +3,17 @@
 // Source http://hutonggames.com/playmakerforum/index.php?topic=10564.0
 
 using UnityEngine;
-using System.Collections;
 
 namespace HutongGames.PlayMaker.Actions
 {
 	[ActionCategory("QualitySettings")]
-	[Tooltip("The number of VSyncs that should pass between each frame. Value must be 0, 1 or 2.")]
+	[Tooltip("The number of VSyncs that should pass between each frame. Value must be 0, 1 2 3 o 4")]
 	[HelpUrl("http://hutonggames.com/playmakerforum/index.php?topic=10564.0")]
 	public class SetVSyncCount : FsmStateAction
 	{
-		
 		[UIHint(UIHint.FsmInt)]
 		public FsmInt level;
 		
-
 		public override void Reset()
 		{
 			level = 2;
@@ -25,22 +22,16 @@ namespace HutongGames.PlayMaker.Actions
 		public override void OnEnter()
 		{
 
-			if (level.Value != 0 || level.Value != 1 || level.Value != 2){
+			if (level.Value >=0 && level.Value<=4){
 
-				Debug.LogError("Value must be 0, 1 or 2");
+				Debug.LogError("Value must be in between 0 and 4");
 				Finish();
 
 			}
-
-
-
 			QualitySettings.vSyncCount = level.Value;
-			
-		
-				Finish();
+			Finish();
 			
 		}
-
 		
 	}
 }
