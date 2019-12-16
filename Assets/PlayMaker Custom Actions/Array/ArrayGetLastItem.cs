905f3532ac1192f8efc07942fc48084f8554c03f
Jean Fabre <contact@fabrejean.net>
2019-12-16 10:20:32 +0300

    New action ArrayGetLastItem

---
+++ Assets/PlayMaker Custom Actions/Array/ArrayGetLastItem.cs
@@ -0,0 +1,71 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+// License: Attribution 4.0 International (CC BY 4.0)
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Array)]
+    [Tooltip("Get a last item of an array")]
+    public class ArrayGetLastItem : FsmStateAction
+    {
+        [RequiredField] 
+        [UIHint(UIHint.Variable)] 
+        [Tooltip("The Array Variable")] 
+        public FsmArray array;
+
+
+
+        [RequiredField]
+        [MatchElementType("array")]
+        [UIHint(UIHint.Variable)] 
+        [Tooltip("Store the last value in a variable.")] 
+        public FsmVar storeValue;
+
+        [Tooltip("The last index in the array.")] 
+        public FsmInt storeIndex;
+        
+
+        public override void Reset()
+        {
+            array = null;
+            storeIndex = null;
+            storeValue = null;
+        }
+
+        public override void OnEnter()
+        {
+            ExecuteAction();
+            
+            Finish();
+        }
+
+        public override void OnUpdate()
+        {
+            ExecuteAction();
+        }
+
+        private void ExecuteAction()
+        {
+            if (array.IsNone)
+            {
+                return;
+            }
+
+            if (!storeValue.IsNone) 
+                storeValue.SetValue(array.Get(array.Length-1));
+            
+            if (!storeIndex.IsNone)
+                storeIndex.Value = array.Length - 1;
+
+        }
+
+
+#if UNITY_EDITOR
+        public override string AutoName()
+        {
+            return ActionHelpers.GetValueLabel(storeValue.NamedVar) + "=" + array.Name + "[" + ActionHelpers.GetValueLabel(storeIndex) + "]";
+        }
+#endif
+    }
+}
