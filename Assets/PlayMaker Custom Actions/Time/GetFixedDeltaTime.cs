83f97b06d8005c259d824a91cf8f8594a990b3e8
Jean Fabre <contact@fabrejean.net>
2019-12-23 15:57:24 +0300

    new action to get fixeddeltatime during fixed update

---
+++ Assets/PlayMaker Custom Actions/Time/GetFixedDeltaTime.cs
@@ -0,0 +1,52 @@
+// (c) Copyright HutongGames, LLC 2010-2019. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Time)]
+	[Tooltip("Get the fixedDeltaTime during the fixedUpdate call")]
+	public class GetFixedDeltaTime : FsmStateAction
+	{
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		[Tooltip("Get the fixed delta time")]
+		public FsmFloat fixedDeltaTime;
+
+		[Tooltip("Repeat every fixed frame.")]
+		public bool everyFixedFrame;
+
+		public override void Reset()
+		{
+			
+			fixedDeltaTime = null;
+			everyFixedFrame = false;
+		}
+
+		public override void OnPreprocess()
+		{
+			Fsm.HandleFixedUpdate = true;
+		}
+		
+		public override void OnEnter()
+		{
+			executeAction();
+
+			if(!everyFixedFrame)
+			{
+				Finish();
+			}
+		}
+
+		public override void OnFixedUpdate()
+		{
+			executeAction();
+		}
+
+		void executeAction()
+		{
+			fixedDeltaTime.Value = Time.fixedDeltaTime;
+		}
+	}
+}
