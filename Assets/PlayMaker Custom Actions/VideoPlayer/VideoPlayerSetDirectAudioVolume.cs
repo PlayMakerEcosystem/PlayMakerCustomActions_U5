d0173d9f37de051e23d90dc9a0ce1fa2d9cd9b3f
Jean Fabre <contact@fabrejean.net>
2020-02-03 10:36:52 +0300

    New Action to set direct audio volume for VideoPLayer.

---
+++ Assets/PlayMaker Custom Actions/VideoPlayer/VideoPlayerSetDirectAudioVolume.cs
@@ -0,0 +1,81 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2020. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+#if UNITY_5_6_OR_NEWER
+
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("VideoPlayer")]
+	[Tooltip("Set the direct-output audio volume for the specified track")]
+	public class VideoPlayerSetDirectAudioVolume : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(VideoPlayer))]
+		[Tooltip("The GameObject with as VideoPlayer component.")]
+		public FsmOwnerDefault gameObject;
+
+		[RequiredField]
+		[Tooltip("The track index")]
+		public FsmInt trackIndex;
+		
+		[RequiredField]
+		[Tooltip("The volume")]
+		public FsmFloat volume;
+
+		[Tooltip("Repeat every frame.")]
+		public bool everyFrame;
+
+		GameObject go;
+
+		VideoPlayer _vp;
+
+
+		public override void Reset()
+		{
+			gameObject = null;
+			trackIndex = null;
+			volume = null;
+			everyFrame = false;
+		}
+
+		public override void OnEnter()
+		{
+			GetVideoPlayer ();
+
+			ExecuteAction ();
+
+			if (!everyFrame)
+			{
+				Finish ();
+			}
+		}
+
+		public override void OnUpdate()
+		{
+			ExecuteAction ();
+		}
+
+
+		void ExecuteAction()
+		{
+			if (_vp != null)
+			{
+				_vp.SetDirectAudioVolume((ushort)trackIndex.Value,volume.Value);
+			}
+		}
+			
+		void GetVideoPlayer()
+		{
+			go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go != null)
+			{
+				_vp = go.GetComponent<VideoPlayer>();
+			}
+		}
+	}
+}
+
+#endif
