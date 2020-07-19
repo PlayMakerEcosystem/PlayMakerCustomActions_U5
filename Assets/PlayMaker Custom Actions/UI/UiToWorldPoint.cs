587e36f94ca4565672c9e82fbd9f6fa317251ae1
DjayDino <jinxtergames@gmail.com>
2020-07-19 17:34:38 +0800

    converted ugui to world point to ui to world point as it was not in the Default UI

---
+++ Assets/PlayMaker Custom Actions/UI/UiToWorldPoint.cs
@@ -0,0 +1,102 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2020. All rights reserved.
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.UI)]
+    [Tooltip("Sets the Main Camera.")]
+    public class UiToWorldPoint : FsmStateAction
+    {
+        [RequiredField]
+        [CheckForComponent(typeof(Camera))]
+        [Tooltip("The GameObject to set as the main camera (should have a Camera component).")]
+        public FsmOwnerDefault gameObjectCamera;
+
+        [RequiredField]
+        [Tooltip("this should be a ui element else you will get strange behaviour")]
+        public FsmGameObject uiElement;
+
+        [ActionSection("Set Target position OR Target Object")]
+        public FsmVector3 targetPosition;
+
+        public FsmGameObject target;
+
+        public FsmVector3 offset;
+
+        [Tooltip("Repeat every frame.")]
+        public bool everyFrame;
+
+        private Vector3 tar;
+
+        public override void Reset()
+        {
+            gameObjectCamera = null;
+            uiElement = null;
+            everyFrame = false;
+            target = new FsmGameObject() { UseVariable = true };
+            targetPosition = new FsmVector3() { UseVariable = true };
+            offset = new FsmVector3() { UseVariable = true };
+        }
+
+        public override void OnEnter()
+        {
+            if (!everyFrame)
+            {
+                DoUiToWorldPoint();
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            DoUiToWorldPoint();
+        }
+
+
+        void DoUiToWorldPoint()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObjectCamera);
+            if (go == null) return;
+
+            if (offset == null || offset.Value == Vector3.zero)
+            {
+                if (!targetPosition.IsNone)
+                {
+                    tar = targetPosition.Value;
+                }
+                else
+                {
+                    if (!target.IsNone)
+                    {
+                        tar = target.Value.transform.position;
+                    }
+                    else return;
+                }
+
+
+            }
+            else
+            {
+                if (!targetPosition.IsNone)
+                {
+                    tar = targetPosition.Value + offset.Value;
+                }
+                else
+                {
+                    if (!target.IsNone)
+                    {
+                        tar = target.Value.transform.position + offset.Value;
+                    }
+                    else return;
+                }
+            }
+
+            Camera _camera = go.GetComponent<Camera>();
+            Vector3 objectPos = _camera.WorldToScreenPoint(tar);
+            uiElement.Value.transform.position = objectPos;
+        }
+    }
+}
