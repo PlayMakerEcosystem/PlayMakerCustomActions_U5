54a4ada8922d6c44d5623bed4306f04d1ca778e4
Romi Fauzan Fauzi <romifauzi@users.noreply.github.com>
2020-08-25 20:18:50 +0700

    Add files via upload
    - added binary save and load action

---
+++ Assets/PlayMaker Custom Actions/Files/BinaryLoad.cs
@@ -0,0 +1,104 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2020. All rights reserved.  
+// License: Attribution 4.0 International(CC BY 4.0)
+// Author: Romi Fauzi
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// Keywords: Load Save Binary Disk
+
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("Files")]
+    [Tooltip("Load Data saved using Binary Save, for Array only type of float, integer, string that are currently working, feel free to test other data types")]
+    public class BinaryLoad : FsmStateAction
+    {
+        [UIHint(UIHint.Variable)]
+        [Tooltip("load your float variable here if there are any saved, can be more than one")]
+        public FsmFloat[] floatVar;
+        [UIHint(UIHint.Variable)]
+        [Tooltip("load your int variable here if there are any saved, can be more than one")]
+        public FsmInt[] intVar;
+        [UIHint(UIHint.Variable)]
+        [Tooltip("load your string variable here if there are any saved, can be more than one")]
+        public FsmString[] stringVar;
+        [UIHint(UIHint.Variable)]
+        [Tooltip("load your bool variable here if there are any saved, can be more than one")]
+        public FsmBool[] boolVar;
+
+        [UIHint(UIHint.Variable)]
+        [Tooltip("load your array variable here, can be more than one and can be a different type (only float, integer and string are currently tested)")]
+        public FsmArray[] arrayVar;
+
+        [Tooltip("name of the save file that will be loaded, make sure the name its the same with the one on Binary Save")]
+        public FsmString filename;
+
+        public FsmEvent notFoundEvent;
+
+        private SavedData loadData = new SavedData();
+
+        public override void Reset()
+        {
+            floatVar = null;
+            intVar = null;
+            stringVar = null;
+            arrayVar = null;
+        }
+
+        public override void OnEnter()
+        {
+            DoLoad();
+
+            Finish();
+        }
+
+        private void DoLoad()
+        {
+            if (File.Exists(Application.persistentDataPath + "/" + filename.Value))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                FileStream file = File.Open(Application.persistentDataPath + "/" + filename.Value, FileMode.Open);
+                loadData = (SavedData)bf.Deserialize(file);
+                file.Close();
+
+                if (loadData.savedFloat.Length > 0)
+                {
+                    for (int i = 0; i < floatVar.Length; i++)
+                        floatVar[i].Value = loadData.savedFloat[i].Value;
+                }
+
+                if (loadData.savedInt.Length > 0)
+                {
+                    for (int i = 0; i < intVar.Length; i++)
+                        intVar[i].Value = loadData.savedInt[i].Value;
+                }
+
+
+                if (loadData.savedString.Length > 0)
+                {
+                    for (int i = 0; i < stringVar.Length; i++)
+                        stringVar[i].Value = loadData.savedString[i].Value;
+                }
+
+                if (loadData.savedBool.Length > 0)
+                {
+                    for (int i = 0; i < stringVar.Length; i++)
+                        boolVar[i].Value = loadData.savedBool[i].Value;
+                }
+
+                if (loadData.savedArray.Length > 0)
+                {
+                    for (int i = 0; i < loadData.savedArray.Length; i++)
+                    {
+                        arrayVar[i].Values = loadData.savedArray[i].Values.Clone() as object[];
+                        arrayVar[i].Resize(arrayVar[i].Length - 1);
+                    }
+                }
+            }
+            else
+                Fsm.Event(notFoundEvent);
+        }
+    }
+}
+
---
+++ Assets/PlayMaker Custom Actions/Files/BinarySave.cs
@@ -0,0 +1,86 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2020. All rights reserved.  
+// License: Attribution 4.0 International(CC BY 4.0)
+// Author: Romi Fauzi
+/*--- __ECO__ __PLAYMAKER__ __ACTION__ ---*/
+// Keywords: Load Save Binary Disk
+
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory("Files")]
+    [Tooltip("Save Data to be load later using Binary Load, for Array only type of float, integer, string that are currently working, feel free to test other data types")]
+    public class BinarySave : FsmStateAction
+    {
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Save your float variable here, can be more than one")]
+        public FsmFloat[] floatVar;
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Save your integer variable here, can be more than one")]
+        public FsmInt[] intVar;
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Save your string variable here, can be more than one")]
+        public FsmString[] stringVar;
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Save your bool variable here, can be more than one")]
+        public FsmBool[] boolVar;
+
+        [UIHint(UIHint.Variable)]
+        [Tooltip("Save your array variable here, can be more than one and can be a different type (only float, integer and string are currently tested)")]
+        public FsmArray[] arrayVar;
+
+        [Tooltip("name of the save file that will be outputted")]
+        public FsmString filename;
+
+        private SavedData saveData = new SavedData();
+
+        public override void Reset()
+        {
+            floatVar = null;
+            intVar = null;
+            stringVar = null;
+            arrayVar = null;
+        }
+
+        public override void OnEnter()
+        {
+            DoSave();
+
+            Finish();
+        }
+
+        private void DoSave()
+        {
+            saveData.savedFloat = floatVar;
+            saveData.savedInt = intVar;
+            saveData.savedString = stringVar;
+            saveData.savedBool = boolVar;
+
+            saveData.savedArray = new FsmArray[arrayVar.Length];
+
+            for (int i = 0; i < saveData.savedArray.Length; i++)
+            {
+                saveData.savedArray[i] = arrayVar[i].Clone() as FsmArray;
+                saveData.savedArray[i].Resize(saveData.savedArray[i].Length + 1);
+            }
+
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Create(Application.persistentDataPath + "/" + filename.Value);
+            bf.Serialize(file, saveData);
+            file.Close();
+        }
+    }
+
+    [System.Serializable]
+    public class SavedData
+    {
+        public FsmFloat[] savedFloat;
+        public FsmInt[] savedInt;
+        public FsmString[] savedString;
+        public FsmBool[] savedBool;
+        public FsmArray[] savedArray;
+    }
+}
+
