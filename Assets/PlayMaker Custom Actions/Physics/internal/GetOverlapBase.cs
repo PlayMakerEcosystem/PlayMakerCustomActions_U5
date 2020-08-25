0e08e02fbfe59ec45e1be5bcbca5c51f97ece623
Romi Fauzan Fauzi <romifauzi@users.noreply.github.com>
2020-08-25 20:25:34 +0700

    Add files via upload
    - added base class GetOverlapBase for GetOverlapBox and GetOverlapSphere

---
+++ Assets/PlayMaker Custom Actions/Physics/internal/GetOverlapBase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.Physics)]
+    public abstract class GetOverlapBase : FsmStateAction
+    {
+        [Tooltip("Use gameObject position as Overlap origin")]
+        public FsmOwnerDefault owner;
+
+        [Tooltip("Use custom position as Overlap origin")]
+        public FsmVector3 fromPosition;
+
+        [RequiredField]
+        [UIHint(UIHint.Variable)]
+        [ArrayEditor(VariableType.GameObject)]
+        [Tooltip("Array to store gameObject(s) overlaps, Array Type has to be GameObject")]
+        public FsmArray array;
+
+        public FsmBool includeOwner;
+
+        [UIHint(UIHint.Layer)]
+        [Tooltip("Layermask name to filter the grabbed objects")]
+        public FsmInt layerMask;
+
+        public FsmEvent onFoundGameObject;
+
+        public bool drawGizmo;
+
+        public override void Reset()
+        {
+            fromPosition = new FsmVector3 { UseVariable = true };
+            drawGizmo = false;
+            array = null;
+        }
+
+        public override void OnEnter()
+        {
+            GetOverlappedGameObject();
+        }
+
+        protected virtual void GetOverlappedGameObject() { }
+    }
+}
